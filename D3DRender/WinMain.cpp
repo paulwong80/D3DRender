79df54b92ccc54e4a54b912a17cbaa6e63773395
paulwong80 <32270732+paulwong80@users.noreply.github.com>
2022-05-19 17:23:43 +0800

    Window draw

--- D3DRender/WinMain.cpp
+++ D3DRender/WinMain.cpp
@@ -1,4 +1,10 @@
 #include <Windows.h>
+#include <iostream>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -6,6 +12,43 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
+	const auto pClassName = "D3D11";
+
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx( &wc );
+
+	// create window
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"D3DWindow",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	// show the window
+	ShowWindow(hWnd, SW_SHOW);
+
+	// message pump
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
 }
